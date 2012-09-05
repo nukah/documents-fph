2e1ed013360c239ed4cfd1ddf909b0501dedd59d
Nuke <flow.energy@gmail.com>
2012-09-05 21:31:36 +0400

    capistrano deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,33 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+require 'bundler/capistrano'
+require 'rvm/capistrano'
 
-set :scm, :subversion
+set :application, "FPH Document Storage"
+set :repository,  "https://github.com/nukah/documents-fph.git"
+set :branch, "master"
+set :git_enable_submodules, 1
+set :scm, :git
+default_run_options[:pty] = true
+set :deploy_to, "/var/www/docs.primepress"
+set :deploy_via, :remote_cache
+set :user, 'user'
+set :use_sudo, false
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, "docs.primepress.ru"                          # Your HTTP server, Apache/etc
+role :app, "docs.primepress.ru"                          # This may be the same as your `Web` server
+role :db,  "docs.primepress.ru", :primary => true # This is where Rails migrations will run
 
 # if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+after "deploy:restart", "deploy:cleanup"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
+namespace :deploy do
 #   task :start do ; end
 #   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+    task :restart, :roles => :app, :except => { :no_release => true } do
+     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+   end
+end
