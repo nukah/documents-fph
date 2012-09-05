29ef0012b1a463a9045da153b9ad01ee48e3c4cb
Nuke <flow.energy@gmail.com>
2012-09-05 20:58:02 +0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Documents::Application.config.secret_token = '7c7e3a86aa30d0a89d350d38372dba44aa55adcd82ffe46fa42396145a2d7ba09d97a60fd5d1c69f0aa8eb47731548b806ff53b644d332f59fb6b14399441548'
